25520da1fdb2ecaa69c946c10b44fcae03ea5194
Patricio Testolin <patricio.testolin@rga.com>
2021-06-19 16:16:07 -0300

    fix mobile

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -1,3 +1,5 @@
+import { ScreenClassRender, Visible } from 'react-grid-system';
+import { breakpoints } from '../../core/constant';
 import { useBasket } from '../../core/context/basket';
 import Item from './item/item';
 
@@ -16,122 +18,126 @@ const Basket = () => {
   }
 
   return (
-    <>
-      <div className="grid">
-        <div className="header">
-          <p>Product</p>
-          <p>Price</p>
-          <p>Qty</p>
-          <p>Cost</p>
-        </div>
-        {basketItems.map((item, index) =>
-          <Item key={index} {...item} />
-        )}
-        {!!basketItems.length || <p className="empty">Cart is Empty</p>}
-      </div>
-      <div className="results">
-        <div className="results-row">
-          <p>Subtotal</p>
-          <span>£{getBasketSubtotal()}</span>
-        </div>
-        <div className="results-row">
-          <p>VAT @20%</p>
-          <span>£{getBasketVat()}</span>
-        </div>
-        <div className="results-row results-row--cost">
-          <p>Total cost</p>
-          <span>£{getTotal()}</span>
-        </div>
-      </div>
-      <div className="btn">
-        <button disabled={!basketItems.length} onClick={() => handlerBuyNow()}>
-          Buy Now <span>{`>>`}</span>
-        </button>
-      </div>
-      <style jsx>
-        {`
-          .header {
-            border-bottom: 1px solid var(--color-black);
-            display: grid;
-            grid-template-columns: 52% repeat(3, 16%);
-            margin-bottom: 10px;
-            padding: 0 20px;
-            text-align: center;
-          }
+    <ScreenClassRender render={screenClass => {
+      return (
+        <>
+          <div className="grid">
+            <div className="header">
+              <p>Product</p>
+              <p>Price</p>
+              <p>Qty</p>
+              <Visible sm md lg xl xxl><p>Cost</p></Visible>
+            </div>
+            {basketItems.map((item, index) =>
+              <Item key={index} {...item} />
+            )}
+            {!!basketItems.length || <p className="empty">Cart is Empty</p>}
+          </div>
+          <div className="results">
+            <div className="results-row">
+              <p>Subtotal</p>
+              <span>£{getBasketSubtotal()}</span>
+            </div>
+            <div className="results-row">
+              <p>VAT @20%</p>
+              <span>£{getBasketVat()}</span>
+            </div>
+            <div className="results-row results-row--cost">
+              <p>Total cost</p>
+              <span>£{getTotal()}</span>
+            </div>
+          </div>
+          <div className="btn">
+            <button disabled={!basketItems.length} onClick={() => handlerBuyNow()}>
+              Buy Now <span>{`>>`}</span>
+            </button>
+          </div>
+          <style jsx>
+            {`
+              .header {
+                border-bottom: 1px solid var(--color-black);
+                display: grid;
+                grid-template-columns: 52% repeat(3, 16%);
+                margin-bottom: 10px;
+                padding: 0 20px;
+                text-align: center;
+              }
 
-          .header p {
-            font-size: 16px;
-            font-weight: 600;
-            line-height: 14px;
-          }
+              .header p {
+                font-size: 16px;
+                font-weight: 600;
+                line-height: 14px;
+              }
 
-          .header p:first-child {
-            text-align: left;
-          }
+              .header p:first-child {
+                text-align: left;
+              }
 
-          .header p:last-child {
-            text-align: right;
-          }
+              .header p:last-child {
+                text-align: ${breakpoints.includes(screenClass) ? 'right' : 'center'};;
+              }
 
-          .results {
-            padding: 30px var(--gutter) 50px;
-          }
+              .results {
+                padding: 30px var(--gutter) 50px;
+              }
 
-          .results-row {
-            color: var(--color-grey-03);
-            display: flex;
-            font-size: 16px;
-          }
+              .results-row {
+                color: var(--color-grey-03);
+                display: flex;
+                font-size: 16px;
+              }
 
-          .results-row span {
-            margin-left: auto;
-          }
+              .results-row span {
+                margin-left: auto;
+              }
 
-          .results-row--cost {
-            color: var(--color-black);
-            font-weight: 600;
-            padding-top: 25px;
-          }
+              .results-row--cost {
+                color: var(--color-black);
+                font-weight: 600;
+                padding-top: 25px;
+              }
 
-          .btn {
-            display: flex;
-            padding: 0 var(--gutter) 50px;
-          }
+              .btn {
+                display: flex;
+                padding: 0 var(--gutter) 50px;
+              }
 
-          .btn button {
-            background: var(--color-blue);
-            border-radius: 10px;
-            border: 0 none;
-            box-shadow: 4px 4px 5px rgb(0 0 0 / 40%);
-            color: var(--color-white);
-            font-size: 16px;
-            font-weight: 600;
-            height: 50px;
-            margin-left: auto;
-            padding: 0 8px;
-          }
+              .btn button {
+                background: var(--color-blue);
+                border-radius: 10px;
+                border: 0 none;
+                box-shadow: 4px 4px 5px rgb(0 0 0 / 40%);
+                color: var(--color-white);
+                font-size: 16px;
+                font-weight: 600;
+                height: 50px;
+                margin-left: auto;
+                padding: 0 8px;
+              }
 
-          .btn button:focus {
-            outline: 1px dotted rgba(0, 0, 0, .3);
-            outline-offset: 4px;
-          }
+              .btn button:focus {
+                outline: 1px dotted rgba(0, 0, 0, .3);
+                outline-offset: 4px;
+              }
 
-          .btn button:disabled {
-            opacity: .4;
-          }
+              .btn button:disabled {
+                opacity: .4;
+              }
 
-          .btn button span {
-            font-size: 12px;
-            letter-spacing: -1px;
-          }
+              .btn button span {
+                font-size: 12px;
+                letter-spacing: -1px;
+              }
 
-          .empty {
-            font-size: 14px;
-            padding: 0 var(--gutter);
-          }
-        `}
-      </style>
-    </>
+              .empty {
+                font-size: 14px;
+                padding: 0 var(--gutter);
+              }
+            `}
+          </style>
+        </>
+      )
+    }} />
   )
 }
 
--- src/components/basket/item/item.js
+++ src/components/basket/item/item.js
@@ -1,3 +1,5 @@
+import { ScreenClassRender, Visible } from 'react-grid-system';
+import { breakpoints } from '../../../core/constant';
 import { useBasket } from '../../../core/context/basket';
 
 const Item = ({id, product, price, qty}) => {
@@ -5,70 +7,74 @@ const Item = ({id, product, price, qty}) => {
   const cost = (price * qty).toFixed(2);
 
   return (
-    <div className="item">
-      <p>{product}</p>
-      <span>£{price}</span>
-      <div>
-        <input
-          type="number"
-          min="1"
-          max="10"
-          value={qty}
-          onChange={(e) => quantityProduct(id, e.target.value)}
-        />
-      </div>
-      <span className="cost">£{cost}</span>
-      <button className="delete" onClick={() => removeProduct(id)}>
-        <img src="images/delete.png" alt="Remove product from Basket" />
-      </button>
-      <style jsx>
-        {`
-          .item {
-            display: grid;
-            font-size: 16px;
-            grid-template-columns: 52% repeat(3, 16%);
-            padding: 8px 20px;
-            position: relative;
-            text-align: center;
-          }
+    <ScreenClassRender render={screenClass => {
+      return (
+        <div className="item">
+          <p>{product}</p>
+          <span>£{price}</span>
+          <div>
+            <input
+              type="number"
+              min="1"
+              max="10"
+              value={qty}
+              onChange={(e) => quantityProduct(id, e.target.value)}
+            />
+          </div>
+          <Visible sm md lg xl xxl><span className="cost">£{cost}</span></Visible>
+          <button className="delete" onClick={() => removeProduct(id)}>
+            <img src="images/delete.png" alt="Remove product from Basket" />
+          </button>
+          <style jsx>
+            {`
+              .item {
+                display: grid;
+                font-size: ${breakpoints.includes(screenClass) ? '16px' : '13px'};
+                grid-template-columns: 52% repeat(3, 16%);
+                padding: 8px 20px;
+                position: relative;
+                text-align: center;
+              }
 
-          .item p:first-child {
-            text-align: left;
-          }
+              .item p:first-child {
+                text-align: left;
+              }
 
-          .item p:last-child {
-            text-align: right;
-          }
+              .item p:last-child {
+                text-align: right;
+              }
 
-          .item:nth-child(odd) {
-            background: var(--color-grey-04);
-          }
+              .item:nth-child(odd) {
+                background: var(--color-grey-04);
+              }
 
-          input {
-            width: 40px;
-            text-align: right;
-          }
+              input {
+                width: ${breakpoints.includes(screenClass) ? '-40px' : '30px'};
+                text-align: right;
+              }
 
-          .cost {
-            text-align: right;
-          }
+              .cost {
+                text-align: right;
+              }
 
-          .delete {
-            position: absolute;
-            top: 6px;
-            right: -30px;
-            width: 30px;
-            height: 30px;
-            border: 0 none;
-            background: transparent;
-          }
+              .delete {
+                position: absolute;
+                top: 6px;
+                right: ${breakpoints.includes(screenClass) ? '-30px' : '15px'};
+                width: 30px;
+                height: 30px;
+                border: 0 none;
+                background: transparent;
+              }
 
-          .delete:focus {
-            outline: 1px dotted rgba(0, 0, 0, .3);
-          }
-        `}
-      </style>
-    </div>
+              .delete:focus {
+                outline: 1px dotted rgba(0, 0, 0, .3);
+              }
+            `}
+          </style>
+        </div>
+      )
+    }} />
   )
 }
 
